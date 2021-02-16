579cb952c678d6f4caf8f22198e684437d5e3317
codervibe <3507557934@qq.com>
2021-02-16 21:14:02 +0800

    修改关于我们的窗体

--- src/main/java/com/codervibe/AboutMeForms.java
+++ src/main/java/com/codervibe/AboutMeForms.java
@@ -11,11 +11,11 @@ import javax.swing.*;
 /**
  * @author Administrator
  */
-public class AboutMeForms extends JFrame{
-    JLabel Text =new JLabel(
+public class AboutMeForms extends JFrame {
+    JLabel Text = new JLabel(
             "版本: 1.0.0.0\n" +
-            "作者: codevibe\n" +
-            "时间: 20210210");
+                    "作者: codevibe\n" +
+                    "时间: 20210210");
 
     public AboutMeForms() {
         super();
@@ -24,14 +24,20 @@ public class AboutMeForms extends JFrame{
         //窗体最上方的标题类似于<title></title>
         setTitle("关于我");
         setLayout(null);
-        getContentPane().add(new JLabel("Just a test."));
 
 
-
-        this.setSize(300,150);
+        setSize(300,150);
         Text.setBounds(20,0,500,40);
         add(Text);
 
 
+
+
+
+
     }
-}
+
+    }
+
+
+
